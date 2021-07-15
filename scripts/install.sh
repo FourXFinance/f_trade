f731590fef1ae340d990f206752fb840048997f2
Warren Fletcher <warren@fletch.band>
2021-07-15 14:02:39 +0200

    Updated installs script to get required libs. This is an ongoing process

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,7 +7,17 @@ cpan ZMQ::FFI
 cpan YAML::XS 
 cpan JSON
 
-# Setup required python Libs/virtual envrionments
+# Setup required python Libs
+# TODO: Proper way to get libs. We should not need sudo?
+
+# Or, we should run as root at all times.
+sudo apt-get -y install python3-pip
+sudo apt-get install libzmq3-dev
+
+
+
+pip3 install python-binance
+pip3 install pyzmq
 
 # Setup required aliases
-alias f='path/to/scripts/f.pl'
+
