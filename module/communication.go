b4ca1e825a28fbf8370e072247fa77cbd0c2a374
Warren Fletcher <me@fletch.band>
2021-07-25 21:30:10 +0200

    Communication Server gets messages from python system

--- module/communication.go
+++ module/communication.go
@@ -1,32 +1,20 @@
 
-//
-//  Hello World client.
-//  Connects REQ socket to tcp://localhost:5555
-//  Sends "Hello" to server, expects "World" back
-//
-
 package main
 
 import (
 	zmq "github.com/pebbe/zmq4"
-
 	"fmt"
 )
 
 func main() {
 	//  Socket to talk to server
 	fmt.Println("Connecting to hello world server...")
-	requester, _ := zmq.NewSocket(zmq.REQ)
+	requester, _ := zmq.NewSocket(zmq.SUB)
 	defer requester.Close()
-	requester.Connect("tcp://localhost:5555")
+	requester.SetSubscribe("0")
+	requester.Connect("tcp://127.0.0.1:11000")
 
-	for request_nbr := 0; request_nbr != 10; request_nbr++ {
-		// send hello
-		msg := fmt.Sprintf("Hello %d", request_nbr)
-		fmt.Println("Sending ", msg)
-		requester.Send(msg, 0)
-
-		// Wait for reply:
+	for {
 		reply, _ := requester.Recv(0)
 		fmt.Println("Received ", reply)
 	}
