34f73154ed0f5c5e1f035484e0c9b6f653feec72
Warren Fletcher <me@fletch.band>
2021-07-25 02:39:50 +0200

    Price Of has support for multiple tickers

--- module/system/binance/priceOf.py
+++ module/system/binance/priceOf.py
@@ -14,24 +14,27 @@ import numpy as np
 import pandas as pd
 
 class PriceOf(BinanceNode):
-    def __init__(self,system_name, target_ticker) -> None:
-        self.ticker = target_ticker
+    def __init__(self,system_name, target_tickers) -> None:
+        self.target_tickers = target_tickers
         self.name = "Trader"
         super().__init__(system_name, self.name)
 
     def run(self):
-        avg_price = self.client.get_avg_price(symbol=self.ticker)
-        if "".join(self.ticker[-4:]) == "USDT":
-            print("\u0024" + str(round(float(avg_price["price"]),2)))
-        elif "".join(self.ticker[-3:]) == "EUR":
-            print("\u20AC" + str(round(float(avg_price["price"]),2)))
+        for ticker in self.target_tickers:
+            avg_price = self.client.get_avg_price(symbol=ticker)
+            if "".join(ticker[-4:]) == "USDT":
+                print("\u0024" + str(round(float(avg_price["price"]),2)), end="")
+            elif "".join(ticker[-3:]) == "EUR":
+                print("\u20AC" + str(round(float(avg_price["price"]),2)), end="")
+            if ticker != self.target_tickers[-1]:
+                print(" ",end="")
             
 if __name__ == "__main__":
     system_name =  sys.argv[1]
-    target_ticker =  sys.argv[2]
+    target_tickers =  sys.argv[2:]
     # Append a UUID to name
     
-    PO = PriceOf(system_name, target_ticker)
+    PO = PriceOf(system_name, target_tickers)
     PO.run()
     
 
