6223dd09a0552be22fa7ac62194e96b350841fe7
Warren Fletcher <me@fletch.band>
2021-07-27 12:54:53 +0200

    Manager Module uses FULL event driven system

--- module/manager.py
+++ module/manager.py
@@ -7,12 +7,11 @@ import time
 import pandas as pd
 from ast import literal_eval
 from enums import AcceptableKlineValues, Sleep
-
 from zmq.eventloop import ioloop, zmqstream
+
 import asyncio
 
 import os
-
 
 class Manager(Node):
     def __init__(self,system_name) -> None:
@@ -22,6 +21,7 @@ class Manager(Node):
         self.ticker_configs = {}
         self.tickers_with_topic = {}
         self.setup()
+        self.build_mappings()
         #TODO: Load Upstream From Config
         #TODO: Load Ticker Starting Ports from Generated Config
         
@@ -30,8 +30,14 @@ class Manager(Node):
         self.setup_upstream()
         self.setup_downstream()
         #TODO Map Bindings of Ticker to Port.
-    def test_message(self, msg):
-        print ("TESTTESTTEST")
+
+    def process_message(self, stream, msg):
+        stream_name = self.upstream_socket_map[stream.socket] # Now we have the name of our stream!
+        print(len(msg))
+        raw_data = msg[0] # For Reaons beyond me, this is an array of data.
+        data = {'topic': raw_data[:1], 'message':raw_data[1:]}
+        message = pd.read_json(data["message"])
+        self.downstream_controller.send_to(stream_name, message.to_json())
     def load_configs(self):
         # Manager Node needs two configs to work: Traders and Market
 
@@ -70,7 +76,7 @@ class Manager(Node):
                     )        
                     socket = self.upstream_controller.get_stream_raw(interval + "." + ticker)
                     stream_sub = zmqstream.ZMQStream(socket)
-                    stream_sub.on_recv(self.test_message)
+                    stream_sub.on_recv_stream(self.process_message)
     def setup_downstream(self):
         for ticker in self.ticker_configs.keys():
             # TODO: Extra Level of Abstraction for Collection of Controllers
@@ -85,30 +91,13 @@ class Manager(Node):
                         register=False
                     )
 
+    
+
     def run(self):
         while True:
             #TODO: Event Loop
             ioloop.IOLoop.instance().start()
             return
-            upstreams = self.upstream_controller.get_streams()
-            downstreams = self.downstream_controller.get_streams()
-            #print(all_streams)
-            upstream_socket_map = {}
-            downstream_name_map = {}
-            for stream in upstreams.keys():
-                #print(all_streams[stream])
-                #print(all_streams[stream].name)
-               upstream_socket_map[upstreams[stream].get_socket()] = upstreams[stream].name
-
-            #print(stream_socket_map)
-            #print(len(self.upstream_controller.recv_snapshot().keys()))
-
-            for stream in downstreams.keys():
-                #print(all_streams[stream])
-                #print(all_streams[stream].name)
-               downstream_name_map[downstreams[stream].name] = downstreams[stream].get_socket()
-
-
             for stream in self.upstream_controller.recv_snapshot():
                 #We have a dictionary of streams. Which one do we have
                 input_stream = upstream_socket_map[stream]
